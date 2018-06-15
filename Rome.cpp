8f5ddfa640330f74e25e6ea108d139a9f955f1f1
CapMidnight <39942999+CapMidnight@users.noreply.github.com>
2018-06-15 13:18:10 +0300

    Add files via upload

---
+++ Rome.cpp
@@ -0,0 +1,21 @@
+//Frome arabic two rome
+// 1= II
+// 0 = I
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+cout<<"Enter number "<< endl;
+cin>>i;
+	string slova[20] =
+		   {
+ 			 "I","II'","III","IV","V","VI","VII","VIII",
+ 			 "VIV","X","XI","XII","XIII","XIV","XV","XVI",
+ 			 "XVII","XVIII","XVIV","XX"
+		   };
+					cout<<slova[i]<< endl;
+						
+					
+}
