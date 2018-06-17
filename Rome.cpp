d6622fdcd2089427d5a77765784f6358526f26b9
CapMidnight <39942999+CapMidnight@users.noreply.github.com>
2018-06-17 15:35:23 +0300

    Update Rome.cpp
    Added many things

--- Rome.cpp
+++ Rome.cpp
@@ -6,16 +6,24 @@
 using namespace std;
 int main()
 {
-int i;
+int i,c;
+c = i;
+char e;
+
+while(c = i){
 cout<<"Enter number "<< endl;
+cout<<"E = Exit"<< endl;
 cin>>i;
-	string slova[20] =
+if(i = e){
+	string slova[30] =
 		   {
  			 "I","II'","III","IV","V","VI","VII","VIII",
  			 "VIV","X","XI","XII","XIII","XIV","XV","XVI",
- 			 "XVII","XVIII","XVIV","XX"
+ 			 "XVII","XVIII","XVIV","XX","XXI","XXII","XXIII",
+ 			 "XXIV","XXV","XXVI","XXVII","XXVIII","XXVIV","XXX"
 		   };
 					cout<<slova[i]<< endl;
-						
+}
+}
 					
 }
