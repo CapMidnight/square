14978355300d6b9233cd94f649c592fd2ad97348
Хардбассер <39942999+CapMidnight@users.noreply.github.com>
2018-06-18 11:14:16 +0300

    Added version 1.5
    Added upload text through '.txt' file

---
+++ Детекор ненормативной лексики/Paul/st. paul.cxx
@@ -0,0 +1,41 @@
+//St. Paul made by Captain Midnight
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <windows.h>
+#include <conio.h>
+#include <clocale>
+#include <fstream>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int c;
+		cout<<"1 - Пошумим?" << endl;
+		cout<<"2 - О программе " << endl;
+		cout<<"3 - Выход " << endl;
+			cin>>c;
+				if(c == 1){
+					setlocale(LC_ALL, "Russian");
+					string buff[326];
+					ifstream F;
+					string c;
+					F.open("C://paul//database.txt", ios::in);
+					cout<<"Enter words"<< endl;
+					F>>buff[326];
+					cin>>c;
+					c = buff[326];
+					cout<<c<< endl;
+					
+				}
+				if(c == 2){
+					cout<<"Привет,это программа для оссуществления поиска по тексту для обнаружения текста" << endl;
+					cout<<"Она еще не доработана. Надеюсь она вам понравистя. :3 " << endl;	
+				}
+				if(c == 3){
+				}
+	return 0;
+}
+
+
